07af7316b35a239b93a4ffbb364f9d34b99d47ef
Paaka <32105930+Paaka@users.noreply.github.com>
2023-04-13 23:00:39 +0200

    test(sign-in-page): created tests for sign up page

--- casia-angular/cypress/e2e/login.cy.ts
+++ casia-angular/cypress/e2e/login.cy.ts
@@ -1,5 +1,44 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+
+  beforeEach(()=>{
+    cy.visit('http://localhost:4200/login');
+    // cy.wait(1000);
+  });
+
+  it('Should contain header', () => {
+    cy.get('[data-cy="header"]').should('be.visible');
+  });
+
+  describe('When email field was clicked', ()=>{
+    beforeEach(()=>{
+      cy.get('[data-cy="email_field"]').click();
+      cy.get('body').click();
+    });
+
+    it('Should set email field as invalid', ()=>{
+      cy.get('.mat-form-field-invalid').should('exist');
+    });
+
+    it('Button for login should be disabled', ()=>{
+      cy.get('[data-cy="sign-up-btn"]').should('be.disabled');
+    });
+
+    describe('When email field is set correctly',()=>{
+      beforeEach(()=>{
+        cy.get('.mat-form-field-invalid').type('test@gmail.com');
+        cy.get('body').click();
+      });
+
+      it('Button for login should be disabled', ()=>{
+        cy.get('[data-cy="sign-up-btn"]').should('be.disabled');
+      });
+
+      it('Should set email field as invalid', ()=>{
+        cy.get('.mat-form-field-invalid').should('not.exist');
+      });
+      
+    });
+
+  });
+
 })
