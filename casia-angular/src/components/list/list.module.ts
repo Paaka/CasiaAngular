2d1c090aed8066f00ac59034f703524bf653d5c8
Mateusz Hartabus <32105930+Paaka@users.noreply.github.com>
2023-04-25 23:57:36 +0200

    fix(list): updated input field to use material

--- casia-angular/src/components/list/list.module.ts
+++ casia-angular/src/components/list/list.module.ts
@@ -1,22 +1,25 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
-import {DragDropModule} from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { ReactiveFormsModule } from '@angular/forms'
+import { DragDropModule } from '@angular/cdk/drag-drop'
 
-import { ListButtonModule } from "../list-button/list-button.module";
-import { ListItemModule } from "../list-item/list-item.module";
-import { ListComponent } from "./list.component";
+import { ListButtonModule } from '../list-button/list-button.module'
+import { ListItemModule } from '../list-item/list-item.module'
+import { ListComponent } from './list.component'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
 
 @NgModule({
-    declarations:[ListComponent],
-    imports:[
+    declarations: [ListComponent],
+    imports: [
         CommonModule,
-
         DragDropModule,
-         ListItemModule, 
-         ListButtonModule, 
-         ReactiveFormsModule
-        ],
-    exports:[ListComponent],
+        ListItemModule,
+        ListButtonModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+    ],
+    exports: [ListComponent],
 })
-export class ListModule{ }
+export class ListModule {}
